259b14af3e2a396492f3df1562bd09937b6412af
Torikul Fauzi <torexfantastic@gmail.com>
2019-04-28 22:09:31 +0700

    Make basic functions.php

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Child Theme Layers
+ * Add define Layers constants to be used around Layers themes, plugins etc.
+ * The current version of the theme inherite to Layers Theme from Elementor. @torexfantastic
+ */
