4d5094b7cd0933d2725320406c447af7954f4389
Torikul Fauzi <torexfantastic@gmail.com>
2019-05-05 04:36:57 +0700

    Delete functions.php

--- functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Child Theme Layers
- * Add define Layers constants to be used around Layers themes, plugins etc.
- * The current version of the theme inherite to Layers Theme from Elementor. @torexfantastic
- */
